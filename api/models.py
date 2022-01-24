3d8663a459a277475c1f9f1de9c826fc5c936899
jefersonvergara <jf.vergara12@gmail.com>
2022-01-23 22:58:21 -0500

    make it better

--- api/models.py
+++ api/models.py
@@ -17,8 +17,8 @@ class Owner(models.Model):
 class Properties(models.Model):
     propietario = models.ForeignKey(Owner, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
-    latitud = models.CharField(max_length=100)
-    longitud = models.CharField(max_length=100)
+    latitud = models.CharField(max_length=100, null=True)
+    longitud = models.CharField(max_length=100, null=True)
     date_created= models.DateTimeField(auto_now_add=True)
     date_updated= models.DateTimeField(auto_now=True)
     
